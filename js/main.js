6e3f176a71e7ed3a2491add7d09b1160733772e5
sadbc1235 <sadbc1235@naver.com>
2022-04-13 18:03:53 +0900

    step1 complet gallery-rwd

--- js/main.js
+++ js/main.js
@@ -1,3 +1,31 @@
+window.addEventListener("load", () => {
+  const grid = new Isotope("section", {
+    itemSelector: "article",
+    columnWidth: "article",
+    transitionDuration: "0.5s",
+  });
 
+  const btns = document.querySelectorAll("main ul li");
 
+  btns.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      e.preventDefault();
 
+      if (btn.classList[0] === "on") {
+        return;
+      }
+      activation(e);
+    });
+  });
+
+  const activation = (e) => {
+    for (let btn of btns) btn.classList.remove("on");
+    e.currentTarget.classList.add("on");
+
+    const btn_a = e.currentTarget.querySelector("a");
+    const a_href = btn_a.getAttribute("href");
+    grid.arrange({
+      filter: a_href,
+    });
+  };
+});
